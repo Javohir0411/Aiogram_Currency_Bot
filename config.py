30f46ecb12bbabe12044491dee399ce848a6f31a
Javohir0411 <javohirabduhakimov979@gmail.com>
2026-01-22 16:36:43 +0500

    Aiogram Currency Bot - tayyor

---
+++ config.py
@@ -0,0 +1,21 @@
+BOT_TOKEN = "8234150250:AAEjYGGfKJ_6HlVSJvCT-f_Nsdw9WcGJHmw"
+
+CBU_URL = 'https://cbu.uz/uz/arkhiv-kursov-valyut/json/'
+
+currencies = {
+    'USD': {
+        'name': 'AQSH dollari',
+        'rate': 12122.07,
+
+    },
+    'EUR': {
+        'name': 'Yevro',
+        'rate': 14187.67,
+
+    },
+    'RUB': {
+        'name': 'Rossiya rubli',
+        'rate': 156.39,
+
+    },
+}
---
+++ main.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
+from aiogram.client.bot import DefaultBotProperties
+from command_handlers import command_router
+from message_handlers import message_router
+from config import BOT_TOKEN
+
+
+async def main():
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(
+            parse_mode=ParseMode.HTML,
+            link_preview_is_disabled=True
+        )
+    )
+    await bot.set_my_commands(
+        commands=[
+            BotCommand(command='start', description='Start/ Botni qayta ishga tushirish'),
+            BotCommand(command='help', description="Botdan foydalanish uchun qo'llanma"),
+            BotCommand(command='courses', description='Hozirgi vaqtdagi kurslar'),
+            BotCommand(command='usd', description='Dollar kursi'),
+            BotCommand(command='eur', description='Yevro kursi'),
+            BotCommand(command='rub', description='Rubl kursi')
+        ]
+    )
+    dp = Dispatcher()
+    dp.include_routers(command_router, message_router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Bot Stopped')
